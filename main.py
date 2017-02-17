042209e5dd186ec34c19755ebcd47b392ae73e45
3100 <sugar16g@gmail.com>
2017-02-17 12:01:50 +0900

    バックアップを作成し、対象ファイルを直接上書き

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
+import glob
 import nkf
 import os
+import shutil
 
 input_dir= './input'
-output_dir= './output'
+backup_dir= './input_bak'
 
 def get_file_names():
-    file_dir_paths = os.listdir(input_dir)
-    return [f for f in file_dir_paths if os.path.isfile(os.path.join(input_dir, f))]
+    return glob.glob(input_dir + '/**/*', recursive=True)
+    #file_dir_paths = os.listdir(input_dir)
+    #return [f for f in file_dir_paths if os.path.isfile(os.path.join(input_dir, f))]
+
+def make_backup():
+    shutil.copytree(input_dir, backup_dir)
 
 def make_output_dir():
-    if os.path.isdir(output_dir):
+    if os.path.isdir(backup_dir):
         return
-    os.mkdir(output_dir)
-    os.chmod(output_dir, 0777)
+    os.mkdir(backup_dir)
+    os.chmod(backup_dir, 0o777)
 
-def write_to_utf8(file_name):
-    input_path = os.path.join(input_dir, file_name)
-    f = open(input_path, 'r')
+# 上書き
+def write_to_utf8(path):
+    f = open(path, 'r')
     str = f.read()
     f.close()
 
     str_utf8 = nkf.nkf('w', str)
-    save_path = os.path.join(output_dir, file_name)
-    fout = open(save_path, 'w')
+    fout = open(path, 'w')
     fout.write(str_utf8)
     fout.close()
 
 if __name__ == '__main__':
-    make_output_dir()
+    make_backup()
+    #make_output_dir()
     file_names = get_file_names()
+    print(file_names)
     for file_name in file_names:
         write_to_utf8(file_name)
