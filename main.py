e7649b139f6f21f4e7b828a3fc719c525a8dff5a
3100 <sugar16g@gmail.com>
2017-02-17 11:44:01 +0900

    出力と入力を分けた

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import nkf
 import os
 
-def get_file_paths():
-    dir_root = '.'
-    file_dir_paths = os.listdir(dir_root)
-    return [f for f in file_dir_paths if os.path.isfile(os.path.join(dir_root, f))]
+input_dir= './input'
+output_dir= './output'
 
-def write_to_utf8(path):
-    f = open(path, 'r')
+def get_file_names():
+    file_dir_paths = os.listdir(input_dir)
+    return [f for f in file_dir_paths if os.path.isfile(os.path.join(input_dir, f))]
+
+def make_output_dir():
+    if os.path.isdir(output_dir):
+        return
+    os.mkdir(output_dir)
+    os.chmod(output_dir, 0777)
+
+def write_to_utf8(file_name):
+    input_path = os.path.join(input_dir, file_name)
+    f = open(input_path, 'r')
     str = f.read()
     f.close()
 
     str_utf8 = nkf.nkf('w', str)
-    fout = open(path, 'w')
+    save_path = os.path.join(output_dir, file_name)
+    fout = open(save_path, 'w')
     fout.write(str_utf8)
     fout.close()
 
 if __name__ == '__main__':
-    paths = get_file_paths()
-    for path in paths:
-        write_to_utf8(path)
-    print(paths)
+    make_output_dir()
+    file_names = get_file_names()
+    for file_name in file_names:
+        write_to_utf8(file_name)
